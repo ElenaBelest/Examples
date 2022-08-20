3419070b8898da166b5319d5c62569cdd9c308ff
Elena <bellenaelena@gmail.com>
2022-08-20 20:51:44 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 1;
+int c = 8;
+int d = 10;
+int e = 6;
+
+int max = a;
+
+if ( a > max) max = a;
+if ( b > max) max = b;
+if ( c > max) max = c;
+if ( d > max) max = d;
+if ( e > max) max = e;
+
+Console.Write ("max = ");
+Console.Write (max);
